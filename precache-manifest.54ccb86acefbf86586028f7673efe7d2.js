8461e59288f9e050852bbc06f3990311228a7a59
Aristotle Poultney <aristotlepoultney@gmail.com>
2018-12-11 19:34:41 -0500

    Updates

---
+++ precache-manifest.54ccb86acefbf86586028f7673efe7d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bb9c9c8d9196810607d",
+    "url": "/contact-manager-redux/static/css/main.a41f0652.chunk.css"
+  },
+  {
+    "revision": "2bb9c9c8d9196810607d",
+    "url": "/contact-manager-redux/static/js/main.2bb9c9c8.chunk.js"
+  },
+  {
+    "revision": "d38f786bc0c8e72d9995",
+    "url": "/contact-manager-redux/static/css/1.ebefb619.chunk.css"
+  },
+  {
+    "revision": "d38f786bc0c8e72d9995",
+    "url": "/contact-manager-redux/static/js/1.d38f786b.chunk.js"
+  },
+  {
+    "revision": "d82270be8b296b4ca8b1",
+    "url": "/contact-manager-redux/static/js/runtime~main.d82270be.js"
+  },
+  {
+    "revision": "aa0a490bfbcf548a39f467ce007e0639",
+    "url": "/contact-manager-redux/index.html"
+  }
+];
